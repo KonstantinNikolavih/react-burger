5a0b0d47f6500e8dd4898735181904f3ae5f97f5
KonstantinNikolavih <82166023+KonstantinNikolavih@users.noreply.github.com>
2022-05-05 00:46:48 +0300

    fix bag version react

--- src/index.jsx
+++ src/index.jsx
@@ -3,19 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App.jsx';
 import reportWebVitals from './reportWebVitals';
-import { compose, createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { rootReducer } from './services/reducers/index.jsx';
-
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-
-const store = createStore(rootReducer, enhancer);
+import { store } from "../src/services/store/store";
 
 ReactDOM.render(
   <React.StrictMode>
